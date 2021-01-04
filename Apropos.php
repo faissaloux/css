b65f42290dcb03526757d35baea3834bf4ccd690
faissaloux <60013703+faissaloux@users.noreply.github.com>
2021-01-04 20:40:06 +0100

    Update: get about page title and content from database

--- Apropos.php
+++ Apropos.php
@@ -20,22 +20,10 @@
         <div class="row">
             <div class="col-md-8 kkksjksjsq" style=''>
                 <h1 class='text-center'>
-                    À propos
+                    <?php echo get_the_title(); ?>
                 </h1>
                 <p>
-                    Caestus Studios vous accompagne et vous propose les meilleurs conseils et solutions pour vos
-                    productions
-                    cinématographiques et audiovisuelles avec des possibilités de livraison à travers tout le Maroc.
-                </p>
-                <p>Notre équipe vous conseille et met en place des prestations adaptées à tous type de production afin
-                    de
-                    vous permettre de faire les meilleurs choix pour concrétiser votre projet de réalisation.</p>
-                <p>Notre parc de location répond à des normes de qualité très strictes avec du matériel neuf et récent
-                    toujours à la pointe avec les cameras les plus performantes de leur génération.
-                </p>
-                <p>
-                    Caestus Studios pourra bientôt répondre à vos demandes de matériel de machinerie et d'éclairage dont
-                    l'offre sera bientôt mise en place.
+                    <?php echo get_the_content(); ?>
                 </p>
             </div>
         </div>
