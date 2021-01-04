dccdf1e82d48bebdaa1153f8ca2a360d1be4ad54
faissaloux <60013703+faissaloux@users.noreply.github.com>
2021-01-04 20:27:59 +0100

    update: add map in about page

--- Apropos.php
+++ Apropos.php
@@ -39,6 +39,11 @@
                 </p>
             </div>
         </div>
+        <div class="row map-container">
+            <div style="width: 100%">
+                <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=R%C3%A9sidence%20Yasmine%20%20Agadir%20,%20Maroc+(Caestus)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+            </div>
+        </div>
     </div>
     <?php require_once('includes/footer.php') ?>
     <?php require_once('includes/scripts.php') ?>
