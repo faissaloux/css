693267500509ac2631204e0d9d5d28864184e510
faissaloux <60013703+faissaloux@users.noreply.github.com>
2021-01-05 01:19:08 +0100

    update: navbar

--- includes/nav.php
+++ includes/nav.php
@@ -1,104 +1,63 @@
 <?php global $theme_setting; ?>
 
 <!-- Navbar -->
-<nav class="navbar navbar-expand-lg  navbar-dark" style="background-color: <?php echo $theme_setting['header-background'] ?>" data-navbar="static">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-8 col-lg-3 navbar-left">
-                <button class="" id='menuphoneopen' type="button">☰</button>
-                <a class="navbar-brand" href="<?php echo get_home_url() ?>">
-                    <img class="logo-dark" src="<?php echo the_logo(); ?>" alt="logo">
-                    <img class="logo-light" src="<?php echo the_logo(); ?>" alt="logo">
-                </a>
-            </div>
-            <section class="col-lg-6 navbar-mobile monstriat">
-                <nav class="nav nav-navbar mx-auto">
-                    <li class="nav-item">
-                        <a class="nav-link ksksks" href="/#materialSection"> Location <span
-                                class="arrow"></span></a>
-                        <ul class="nav">
-                            <li class="nav-item">
-                                <a class="nav-link" href="/pack-production">Packs Production</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/cameralist">Caméras <span class="arrow"></span> </a>
-                                <nav class="nav">
-                                    <a class="nav-link" href="/red"> RED </a>
-                                </nav>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/optiques">Optiques <span class="arrow"></span> </a>
-                                <nav class="nav">
-                                    <a class="nav-link" href="/optiques#sigma"> SIGMA </a>
-                                    <a class="nav-link" href="/optiques#canoon"> CANON </a>
-                                </nav>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/Steadicams-Stabilisateurs">Steadicams / Stabilisateurs
-                                    <span class="arrow"></span></a>
-                                <nav class="nav">
-                                    <a class="nav-link" href="/category/25"> Steadicams </a>
-                                    <a class="nav-link" href="/category/24">Stabilisateurs </a>
-                                </nav>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/Trepieds-SupportsCamera">Supports Caméras / Trépieds<span
-                                        class="arrow"></span></a>
-                                <nav class="nav">
+<nav class="navbar navbar-expand-md" style="background-color: <?php echo $theme_setting['header-background'] ?>; position: block;">
 
-                                    <a class="nav-link" href="/category/26"> Supports Caméra </a>
-                                    <a class="nav-link" href="/category/27"> Trépieds </a>
-                                </nav>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/accessoires">Accessoires <span class="arrow"></span></a>
-                                <nav class="nav">
-                                    <a class="nav-link" href="/category/14"> Accessoires RED </a>
-                                    <a class="nav-link" href="/category/13"> Accessoires Camera </a>
-                                    <a class="nav-link" href="/category/19"> Moniteurs Enregistreurs </a>
-                                    <a class="nav-link" href="/category/17"> Follow Focus </a>
-                                    <a class="nav-link" href="/category/15"> Alimentation Et Batteries </a>
-                                    <a class="nav-link" href="/category/18"> Media Cartes Mémoires </a>
-                                    <a class="nav-link" href="/category/16">Filtres</a>
-                                    <a class="nav-link" href="/category/20">Roulante ADICAM MAX </a>
-                                </nav>
-                            </li>
-                        </ul>
-                    </li>
-                    </a>
-                    <li class="nav-item">
-                        <a class="nav-link ksksks" href="<?php echo get_home_url().'/services'; ?>"> <span>Prestation</span>
-                            <span class="arrow"></span> </a>
-                        <ul class="nav">
-                            <a class="nav-link" href="/prestation/8">Opérateur Steadicam</a>
-                            <a class="nav-link" href="/prestation/7">Opérateur Dji Ronin 2</a>
-                            <a class="nav-link" href="/prestation/6">Techniciens</a>
-                            <a class="nav-link" href="/prestation/5">Transport</a>
-                    </li>
-                    </ul>
-                    </li>
-                    <li class="nav-item"><a class="nav-link ksksks" href="<?php echo get_home_url().'/a-propos'; ?>">
-                            <span>À propos</span> </a></li>
-            </section>
-            <div class="col-4 col-lg-3 text-right lbotonat">
-                <div class="search-For-Top-Header">
-                    <input type="text" class='searchStuf' style='display:none;' placeholder="Recherche">
-                    <ul class='mlsqfkkfmkmlklm' style='display:none;'>
-                        <li class='no-reuslt'>Aucun Resultat</li>
-                    </ul>
-                </div>
-                <a class="btn btn-xs ssfsqzgzagez" href='javascript:;' data-toggle="offcanvas"
-                    data-target="#offcanvas-search">
-                    <span class="ti-search"></span></a>
-                <div class="carter"><a class="btn btn-xs" id="cart" style="background-color: <?php echo $theme_setting['header-cart-background'] ?>" href="<?php echo get_home_url().'/cart'; ?>"> 0 <span
-                            class="icon-document"></span></a>
-                </div>
-                <a class="btn btn-xs" href="https://goo.gl/maps/1zkbzfxZeiAUGqCX9"> <span
-                        class="ti-location-pin"></span></a>
-            </div>
+    <div class="container">
+        <a class="navbar-brand" href="<?php echo get_home_url() ?>" target="_blank"><img src="<?php echo the_logo(); ?>" alt=""></a>	
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse col-lg-6" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto py-4 py-md-0">
+                <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4 active">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Location</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Packs</a>
+                        <a class="dropdown-item" href="#">Caméras</a>
+                        <a class="dropdown-item" href="#">Optiques</a>
+                        <a class="dropdown-item" href="#">Steadicams / Stabilisateurs</a>
+                        <a class="dropdown-item" href="#">Supports Caméras / Trépieds</a>
+                        <a class="dropdown-item" href="#">Accessoires</a>
+                    </div>
+                </li>
+                <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="<?php echo get_home_url().'/services'; ?>" role="button" aria-haspopup="true" aria-expanded="false">Prestation</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Opérateur Steadicam</a>
+                        <a class="dropdown-item" href="#">Opérateur Dji Ronin 2</a>
+                        <a class="dropdown-item" href="#">Techniciens</a>
+                        <a class="dropdown-item" href="#">Transport</a>
+                    </div>
+                </li>
+                <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
+                    <a class="nav-link" href="<?php echo get_home_url().'/a-propos'; ?>">À PROPOS</a>
+                </li>
+            </ul>
         </div>
+
+        <div class="col-4 col-lg-3 text-right lbotonat">
+            <div class="search-For-Top-Header">
+                <input type="text" class='searchStuf' style='display:none;' placeholder="Recherche">
+                <ul class='mlsqfkkfmkmlklm' style='display:none;'>
+                    <li class='no-reuslt'>Aucun Resultat</li>
+                </ul>
+            </div>
+            <a class="btn btn-xs ssfsqzgzagez" href='javascript:;' data-toggle="offcanvas"
+                data-target="#offcanvas-search">
+                <span class="ti-search"></span></a>
+            <div class="carter"><a class="btn btn-xs" id="cart" style="background-color: <?php echo $theme_setting['header-cart-background'] ?>" href="<?php echo get_home_url().'/cart'; ?>"> 0 <span
+                        class="icon-document"></span></a>
+            </div>
+            <a class="btn btn-xs" href="https://goo.gl/maps/1zkbzfxZeiAUGqCX9"> <span
+                    class="ti-location-pin"></span></a>
+        </div>
+    
     </div>
-</nav>
+</nav>		
+			
 
 
 <div class="container">
