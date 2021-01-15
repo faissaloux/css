4bc7d4e0a8261cfd4727f758c563228500a6ac61
faissaloux <60013703+faissaloux@users.noreply.github.com>
2021-01-15 22:44:42 +0100

    update: 404 page

--- 404.php
+++ 404.php
@@ -1,27 +1,31 @@
-<!-- جلب الهيدر -->
-<?php  get_header();  ?>
+<?php
+    define( 'HOME_DIR', get_template_directory(). '/templates/home');
+    global $theme_setting;
 
-<!-- جلب اعدادات القالب من ريداكس فريمورك -->
-<?php global $theme_setting;  ?>
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-<!-- جلب محتوى الهيدر -->
-<?php get_main_header(); ?>  
+<head>
+    <?php require_once('includes/head.php') ?>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/app.css">
+</head>
+<a href="javascript:;" id="button" style="background-color: <?php echo $theme_setting['top-arrow-color'] ?>"></a>
 
-<!-- مسار الصفحة -->
-<?php the_breadcrumb(); ?>
+<body>
+    <?php require_once 'includes/nav.php' ?>
+    <div class="page-not-found">
+        <div class="page-not-found-code d-flex justify-content-center align-items-center">
+            <span>4</span>
+            <div class="img-container">
+                <img src="<?php echo get_template_directory_uri();?>/assets/images/404/404.png" />
+            </div>
+            <span>4</span>
+        </div>
+        <p class="page-not-found-text">Page not found!</p>
+    </div>
+    <?php require_once 'includes/footer.php'; ?>
+    <?php require_once 'includes/scripts.php'; ?>
+</body>
 
-<?php echo get_home_url();  ?>
-
-
-<form action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get" role="search">
-    <input type="text" name="s" placeholder="Search">
-    <button type="type"><i class="fas fa-search"></i></button>
-</form>
-
-
-
-<!-- جلب محتوى الفوتر -->
-<?php get_main_footer(); ?>
-
-<!-- جلب الفوتر -->
-<?php get_footer(); ?>
+</html>
