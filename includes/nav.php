67fe3a38d6fad0ca95df018792872c6c10a4f598
faissaloux <60013703+faissaloux@users.noreply.github.com>
2021-01-21 20:33:49 +0100

    update: responsive navbar

--- includes/nav.php
+++ includes/nav.php
@@ -7,39 +7,14 @@
 <nav class="navbar nav-custom navbar-expand-md" style="background-color: <?php echo $theme_setting['header-background'] ?>;">
 
     <div class="container">
-        <a href="#my-menu" class="show-mobile show-menu col-4"><i class="fas fa-bars"></i></a> 
-        <a class="navbar-brand col-3" href="<?php echo get_home_url() ?>" target="_blank"><img src="<?php echo the_logo(); ?>" alt=""></a>	
+        <a href="#my-menu" class="show-mobile show-menu col-3"><i class="fas fa-bars"></i></a> 
+        <a class="navbar-brand col-sm-2 col-md-2 px-0" href="<?php echo get_home_url() ?>" target="_blank"><img src="<?php echo the_logo(); ?>" alt=""></a>	
         
-        <div class="collapse navbar-collapse col-lg-6" id="navbarSupportedContent">
-        <?php top_header_menu_bootstrap4(); ?>
-            <!-- <ul class="navbar-nav mx-auto py-4 py-md-0">
-                <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4 active">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="<?php echo get_home_url().'/materialSection'; ?>" role="button" aria-haspopup="true" aria-expanded="false">Location</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="<?php echo get_home_url().'/pack-production' ?>">Packs</a>
-                        <a class="dropdown-item" href="<?php echo get_home_url().'/cameralist' ?>">Caméras</a>
-                        <a class="dropdown-item" href="<?php echo get_home_url().'/optiques' ?>">Optiques</a>
-                        <a class="dropdown-item" href="<?php echo get_home_url().'/Steadicams-Stabilisateurs' ?>">Steadicams / Stabilisateurs</a>
-                        <a class="dropdown-item" href="<?php echo get_home_url().'/Trepieds-SupportsCamera' ?>">Supports Caméras / Trépieds</a>
-                        <a class="dropdown-item" href="<?php echo get_home_url().'/accessoires' ?>">Accessoires</a>
-                    </div>
-                </li>
-                <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="<?php echo get_home_url().'/services'; ?>" role="button" aria-haspopup="true" aria-expanded="false">Prestation</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="<?php echo get_home_url().'/prestation/8' ?>">Opérateur Steadicam</a>
-                        <a class="dropdown-item" href="<?php echo get_home_url().'/prestation/7' ?>">Opérateur Dji Ronin 2</a>
-                        <a class="dropdown-item" href="<?php echo get_home_url().'/prestation/6' ?>">Techniciens</a>
-                        <a class="dropdown-item" href="<?php echo get_home_url().'/prestation/5' ?>">Transport</a>
-                    </div>
-                </li>
-                <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-                    <a class="nav-link" href="<?php echo get_home_url().'/a-propos'; ?>">À PROPOS</a>
-                </li>
-            </ul> -->
+        <div class="collapse navbar-collapse col col-lg-6" id="navbarSupportedContent">
+            <?php top_header_menu_bootstrap4(); ?>
         </div>
 
-        <div class="col-4 col-md-4 col-lg-3 text-right lbotonat">
+        <div class="col-4 col-md-3 col-lg-3 text-right lbotonat d-flex align-items-center">
             <div class="ui-dialog ui-widget ui-widget-content ui-corner-all ui-front ox-dialog ox-slideout ox-slideout-top active ox-slideout-active" tabindex="-1" role="dialog" aria-describedby="ui-id-1" style="display: none;">
             <div class="block block-content block-content-slideout ui-dialog-content ui-widget-content ox-modal-content" data-move-mobile="search.slideout" data-move-sticky="search.slideout" style="display: block;" data-role="OXmodal" id="ui-id-1">
 		<div class="header__search-wrapper ox-move-item ox-move-item-search.slideout">
