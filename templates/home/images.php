f5ab35700892b1ff616692bb30ab57d9016aa7d3
faissaloux <60013703+faissaloux@users.noreply.github.com>
2021-01-25 21:59:45 +0100

    fix: index images in small screen

--- templates/home/images.php
+++ templates/home/images.php
@@ -6,7 +6,7 @@
         <!-- Main Content -->
         <main class="main-content" id='materialSection'>
             <div class="row qkj">
-                <div class="col-md-61">
+                <div class="col-md-61 home-image">
                     <a href="<?php echo get_home_url().$theme_setting['image1-url']; ?>">
                         <div class="boxImageCamera">
                             <img src="<?php print_r($theme_setting['image1']['url']); ?>"
@@ -17,7 +17,7 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-md-61">
+                <div class="col-md-61 home-image">
                     <a href="<?php echo get_home_url().$theme_setting['image2-url']; ?>">
                         <div class="boxImageCamera">
                             <img src="<?php print_r($theme_setting['image2']['url']); ?>"
@@ -29,7 +29,7 @@
                 </div>
             </div>
             <div class="row qkj">
-                <div class="col-md-31">
+                <div class="col-md-31 home-image">
                     <a href="<?php echo get_home_url().$theme_setting['image3-url']; ?>">
                         <div class="boxImageCamera"><img class="images-img" src="<?php print_r($theme_setting['image3']['url']); ?>" />
                             <div class="ghakteb"><?php echo $theme_setting['image3-title']; ?></div>
@@ -37,21 +37,21 @@
                     </a>
                 </div>
 
-                <div class="col-md-31">
+                <div class="col-md-31 home-image">
                     <a href="<?php echo get_home_url().$theme_setting['image4-url']; ?>">
                         <div class="boxImageCamera"><img class="images-img" src="<?php print_r($theme_setting['image4']['url']); ?>" />
                             <div class="ghakteb"><?php echo $theme_setting['image4-title']; ?></div>
                         </div>
                     </a>
                 </div>
-                <div class="col-md-31">
+                <div class="col-md-31 home-image">
                     <a href="<?php echo get_home_url().$theme_setting['image5-url']; ?>">
                         <div class="boxImageCamera"><img class="images-img" src="<?php print_r($theme_setting['image5']['url']); ?>" />
                             <div class="ghakteb"><?php echo $theme_setting['image5-title']; ?></div>
                         </div>
                     </a>
                 </div>
-                <div class="col-md-31">
+                <div class="col-md-31 home-image">
                     <a href="<?php echo get_home_url().$theme_setting['image6-url']; ?>">
                         <div class="boxImageCamera"><img class="images-img" src="<?php print_r($theme_setting['image6']['url']); ?>" />
                             <div class="ghakteb"><?php echo $theme_setting['image6-title']; ?></div>
