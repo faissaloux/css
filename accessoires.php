934112691e3e51d5251654f591413f96048d1af0
faissaloux <60013703+faissaloux@users.noreply.github.com>
2021-01-25 13:45:48 +0100

    update: get accessories from db

--- accessoires.php
+++ accessoires.php
@@ -24,86 +24,20 @@
     <!-- Main Content -->
     <main class="main-content">
         <div class="row secondRow">
-            <div class="col-md-3">
-                <a href="/category/14">
-                    <div class="boxImageCamera"><img src="<?php echo get_template_directory_uri();?>/assets/images/accessoires/accessoire_red.jpg" />
-                        <div class="ghakteb">ACCESSOIRES RED</div>
+
+            <?php foreach($accessories as $accessory): ?>
+                <?php if( !empty( $accessory['image'] ) ): ?>
+                    <div class="col-md-3">
+                        <a href="<?php echo $accessory['url']; ?>">
+                            <div class="boxImageCamera">
+                                <img src="<?php echo $accessory['image']; ?>" />
+                                <div class="ghakteb"><?php echo $accessory['title']; ?></div>
+                            </div>
+                        </a>
                     </div>
-                </a>
-            </div>
-
-
-            <div class="col-md-3">
-                <a href="/category/13">
-                    <div class="boxImageCamera"><img src="<?php echo get_template_directory_uri();?>/assets/images/accessoires/accessoires_camera.jpg" />
-                        <div class="ghakteb">ACCESSOIRES CAMERA</div>
-
-
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a href="/category/19">
-                    <div class="boxImageCamera"><img src="<?php echo get_template_directory_uri();?>/assets/images/accessoires/MONITEURS_ENREGISTREUR.jpg" />
-                        <div class="ghakteb">MONITEURS ENREGISTREURS</div>
-
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a href="/category/17">
-                    <div class="boxImageCamera"><img src="<?php echo get_template_directory_uri();?>/assets/images/accessoires/FOLLOW_FOCUS.jpg" />
-                        <div class="ghakteb">FOLLOW FOCUS</div>
-
-                    </div>
-                </a>
-            </div>
-
-
-            <div class="col-md-3">
-                <a href="/category/15">
-                    <div class="boxImageCamera"><img src="<?php echo get_template_directory_uri();?>/assets/images/accessoires/BATTERIES_ET_ALIMENTATION.jpg" />
-                        <div class="ghakteb">ALIMENTATION ET BATTERIES</div>
-
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a href="/category/18">
-                    <div class="boxImageCamera"><img src="<?php echo get_template_directory_uri();?>/assets/images/accessoires/MEDIA_CARTE_MEMOIRES.jpg" />
-                        <div class="ghakteb">MEDIA CARTES MÉMOIRES</div>
-
-                    </div>
-                </a>
-            </div>
-
-
-            <div class="col-md-3">
-                <a href="/category/16">
-                    <div class="boxImageCamera"><img src="<?php echo get_template_directory_uri();?>/assets/images/accessoires/FILTRES.jpg" />
-                        <div class="ghakteb">FILTRES</div>
-
-                    </div>
-                </a>
-            </div>
-
-
-            <div class="col-md-3">
-                <a href="/category/20">
-                    <div class="boxImageCamera"><img src="<?php echo get_template_directory_uri();?>/assets/images/accessoires/ROULANTE_ADICAM_MAX.jpg" />
-                        <div class="ghakteb">ROULANTE ADICAM MAX</div>
-
-                    </div>
-                </a>
-            </div>
-
-
+                <?php endif; ?>
+            <?php endforeach; ?>
         </div>
-
-
     </main>
     
     <?php require_once('includes/footer.php') ?>
