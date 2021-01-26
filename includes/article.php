9d7d03a6d38c41959ba44b6f29e305951eaff66c
faissaloux <60013703+faissaloux@users.noreply.github.com>
2021-01-26 10:57:54 +0100

    update: remove picture in single article page

--- includes/article.php
+++ includes/article.php
@@ -6,9 +6,6 @@
         <div class="row">
             <div class="col">
                 <h1 class="single-post-title text-left"><?php echo the_title(); ?></h1>
-                <div class="single-post-img-cont">
-                    <img src="<?php echo get_the_post_thumbnail_url();?>" class="mt-3">
-                </div>
                 <div class="__desc-post my-3 single-post-content">
                     <p><?php echo the_content(); ?></p>
                 </div>
